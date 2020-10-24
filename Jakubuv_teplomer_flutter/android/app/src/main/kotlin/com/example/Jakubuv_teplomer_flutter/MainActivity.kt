64e9a3474691af89071881c35b496f0477a06be6
Jakub Sirový <38387772+jakubsirovy@users.noreply.github.com>
2020-10-24 23:46:06 +0200

    Repair

---
+++ Jakubuv_teplomer_flutter/android/app/src/main/kotlin/com/example/Jakubuv_teplomer_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Jakubuv_teplomer_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- Jakubův_teploměr_flutter/android/app/src/main/kotlin/com/example/json_api_fetch/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.json_api_fetch
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
